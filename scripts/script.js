4d7c218ea71df854252d493f6213c13952eeca1b
Kevin Le <kevinle108@gmail.com>
2020-06-02 10:08:00 -0400

    add js functionality w/ mpg app

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
+let user_range = 0;
+let user_gas = 0;
 
-document.querySelector('button').onclick = function() {
-    alert('Hey, you clicked me!');
+
+let range = document.querySelector('.rangeText');
+let gas = document.querySelector('.gasText');
+let mpg = document.querySelector('.mpgText');
+
+document.querySelector('button').onclick = testfunction;
+
+function testfunction() {
+    let user_range = prompt("How many miles travelled?");
+    let user_gas = prompt("How many gallons of gas was used?");
+    let mpgCalculated = Math.round(user_range / user_gas);
+    range.textContent = user_range + " miles";
+    gas.textContent = user_gas + " gallons";
+    mpg.textContent = mpgCalculated + " miles/gallon";
 }
