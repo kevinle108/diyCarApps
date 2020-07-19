799f9d81940e1671b7db45f33c8f49d85d4ab2e4
Kevin Le <kevinle108@gmail.com>
2020-07-18 23:42:59 -0400

    revised layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,31 @@
-let user_range = 0;
-let user_gas = 0;
+// let user_range = 0;
+// let user_gas = 0;
 
 
-let range = document.querySelector('.rangeText');
-let gas = document.querySelector('.gasText');
-let mpg = document.querySelector('.mpgText');
+// let range = document.querySelector('.rangeText');
+// let gas = document.querySelector('.gasText');
+// let mpg = document.querySelector('.mpgText');
 
-document.querySelector('button').onclick = testfunction;
+// document.querySelector('button').onclick = testfunction;
 
-function testfunction() {
-    let user_range = prompt("How many miles travelled?");
-    let user_gas = prompt("How many gallons of gas was used?");
-    let mpgCalculated = Math.round(user_range / user_gas);
-    range.textContent = user_range + " miles";
-    gas.textContent = user_gas + " gallons";
-    mpg.textContent = mpgCalculated + " miles/gallon";
-}
+// function testfunction() {
+//     let user_range = prompt("How many miles travelled?");
+//     let user_gas = prompt("How many gallons of gas was used?");
+//     let mpgCalculated = Math.round(user_range / user_gas);
+//     range.textContent = user_range + " miles";
+//     gas.textContent = user_gas + " gallons";
+//     mpg.textContent = mpgCalculated + " miles/gallon";
+// }
+
+// html code: 
+{/* <div class="mpgApp">       
+        <h2>MPG Calculator App:</h2> <button >Click to calculate mpg!</button>
+        <!-- there are blank spaces inside rangeText and gasText divs -->
+        <p>Range (in miles):</p> <div class="rangeText"> </div> 
+        <p>Gas consumed (in gallons):</p> <div class="gasText"> </div>
+        <p>Calculated MPG:</p> 
+        <div class="mpgText">0 mile/gallon</div>
+    </div> */}
 
 // js code for form //
 
@@ -56,15 +66,22 @@ document.querySelector('#btnShowForm').onclick = showHideForm;
 function showHideForm() {
   let x = document.getElementById('myDIV');
   if (x.style.display === 'none') {
-    x.style.display = 'block';
+    x.style.display = 'flex';
   } else {
     x.style.display = 'none';
   }
 }
 
-let showHideMenuBtn = document.getElementById('showButtonNavBar');
+const showHideMenuBtn = document.getElementById('showButtonNavBar');
 showHideMenuBtn.addEventListener('click', showHandler);
 function showHandler(event) {
   event.preventDefault();
   showHideForm();
+}
+
+const contactMenuBtn = document.getElementById('contactMenuItem');
+contactMenuBtn.addEventListener('click', contactBtnHandler);
+function contactBtnHandler(event) {
+  event.preventDefault();
+  alert('Kevin Le: kevinle108@gmail.com');
 }
