8c8cb563616d6803ee51e9ad7539b1b53e34bddf
Kevin Le <kevinle108@gmail.com>
2020-07-26 16:35:45 -0400

    changed to multiple page layout

--- readme.md
+++ readme.md
@@ -1,1 +1,32 @@
-Hello World, testing readme file...
+# Code Louisville Front-End Project
+
+## Main features
+Showcases an understanding of HTML, CSS, and JS to build a webpage.
+
+## Feature Requirements met
+- A navigation menu that collapses into a responsive mobile-friendly menu (such as a “hamburger” menu) 
+- Create a form that takes in at least two fields of information and validates input (for example, it checks to ensure email is the a@b.com format
+- Create a JavaScript function whose return value is used in your site. The function must be triggered by user action (ex: clicking a button).
+- Use JavaScript to perform math operations and display the result on your site.
+- Use “:nth child” CSS to style a series of elements (for example, change the color of every 2nd item in a list).
+
+## Additional Requirements met
+- Your project is responsive to different devices and/or browser sizes and improves the user experience in some way
+- - We will look at your app on a desktop screen and a mobile device to see what changes occur
+- - We must be able to see the change that your code is making on your website - a media query can be written correctly but have no actual effect. That will not be ok in this class.
+- Your project includes at minimum 3 custom-written CSS elements (selectors, classes, etc) located in a .css file which are applied to your HTML elements
+The CSS must be included in its own file, not “in-line” on your HTML page - we want to see that you can properly “link” a stylesheet
+Many of the options in the first requirement will also fulfil this requirement
+- Your project includes JavaScript in a separate .js file
+Script tags in your HTML file must execute the JS file
+Many of the options in the first requirement will also fulfil this requirement
+- Your code have comments that document major sections of your code to make it easier to read
+You don’t need to go crazy on this - a few code comments are perfectly fine
+- Your project code is uploaded to your GitHub account, in its own repository, with at least 5 commits
+We need to see that you’ve used Git to update your GitHub profile at least 5 times
+Using GitHub’s file uploader does not count as a check-in. You must upload via Git. 
+- It must include a README file located at the top level directory of your project that includes:
+A description of your project
+What features you chose to included (so we know what to look for)
+Any special instructions we might need to run your project
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,87 +1,20 @@
-// let user_range = 0;
-// let user_gas = 0;
-
-
-// let range = document.querySelector('.rangeText');
-// let gas = document.querySelector('.gasText');
-// let mpg = document.querySelector('.mpgText');
-
-// document.querySelector('button').onclick = testfunction;
-
-// function testfunction() {
-//     let user_range = prompt("How many miles travelled?");
-//     let user_gas = prompt("How many gallons of gas was used?");
-//     let mpgCalculated = Math.round(user_range / user_gas);
-//     range.textContent = user_range + " miles";
-//     gas.textContent = user_gas + " gallons";
-//     mpg.textContent = mpgCalculated + " miles/gallon";
-// }
-
-// html code: 
-{/* <div class="mpgApp">       
-        <h2>MPG Calculator App:</h2> <button >Click to calculate mpg!</button>
-        <!-- there are blank spaces inside rangeText and gasText divs -->
-        <p>Range (in miles):</p> <div class="rangeText"> </div> 
-        <p>Gas consumed (in gallons):</p> <div class="gasText"> </div>
-        <p>Calculated MPG:</p> 
-        <div class="mpgText">0 mile/gallon</div>
-    </div> */}
-
-// js code for form //
-
-
 document.querySelector('#form-submit').onclick = submitFormMPG;
-
 function submitFormMPG() {
-    let formRange = document.querySelector('#form-mpg-range').value;
-    let formGas = document.querySelector('#form-mpg-gas').value;
-    if (Number(formRange) && Number(formGas)) {
-        formRange = +formRange;
-        formGas = +formGas;
-        let answer = Math.round(formRange / formGas);
-        console.log(`Range was ${formRange} miles`);
-        console.log(`Gas was ${formGas} gallons`);
-        console.log(`Answer is ${answer} mpg`);
-        document.querySelector('#form-mpg-result').value = `${answer} miles / gallon`;        
-    } else {
-        alert('Error: Invalid inputs were submitted. Please enter numbers to calculate your mpg.')
-    }
-}
-
-//code for show / hide form 
-
-// document.querySelector('#btnOpen-mpgform').onclick = showHide;
-
-// function showHide() {
-//     // let form = document.querySelector('.mpgform-wrap');
-//     // if (form.style.display === 'none') {
-//     //     form.style.display === 'block';
-//     // } else {
-//     //     form.style.display === 'none';
-//     // }
-//     document.querySelector('.formtitle').textContent = 'CLICKED!';
-// }
-
-document.querySelector('#btnShowForm').onclick = showHideForm;
-function showHideForm() {
-  let x = document.getElementById('myDIV');
-  if (x.style.display === 'none') {
-    x.style.display = 'flex';
+  let formRange = document.querySelector('#form-mpg-range').value;
+  let formGas = document.querySelector('#form-mpg-gas').value;
+  if (Number(formRange) && Number(formGas)) {
+      distance = +formRange;
+      gas = +formGas;
+      let answer = calculateMPG(distance, gas);
+      console.log(`Range was ${formRange} miles`);
+      console.log(`Gas was ${formGas} gallons`);
+      console.log(`Answer is ${answer} mpg`);
+      document.querySelector('#form-mpg-result').value = `${answer} miles / gallon`;        
   } else {
-    x.style.display = 'none';
+      alert('Error: Invalid inputs were submitted. Please enter numbers to calculate your mpg.')
   }
 }
 
-const showHideMenuBtn = document.getElementById('showButtonNavBar');
-showHideMenuBtn.addEventListener('click', showHandler);
-function showHandler(event) {
-  event.preventDefault();
-  showHideForm();
-}
-
-const contactMenuBtn = document.getElementById('contactMenuItem');
-contactMenuBtn.addEventListener('click', contactBtnHandler);
-function contactBtnHandler(event) {
-  event.preventDefault();
-  alert('Kevin Le: kevinle108@gmail.com');
+function calculateMPG(miles, gallons) {
+  return Math.round(miles / gallons);
 }
